d07bb0f0455dc9183dd51452b027a46e350e97d5
robbytrujillo <robbyikusuma88@gmail.com>
2024-11-29 06:02:48 +0700

    introduction express-api | connection posts

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello Robby')
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello Robby')
+// })
+
+// import route posts
+const postsRoute = require('./routes/posts');
+app.use('/api/posts', postsRouter);
 
 app.listen(port, () => {
     console.log(`app running at http://localhost:${port}`)
